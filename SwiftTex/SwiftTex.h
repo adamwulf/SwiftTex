026d0384cd52d3bce6db78900514a21730e122fb
Adam Wulf <adam.wulf@gmail.com>
2020-11-30 17:25:28 -0600

    initial commit

---
+++ SwiftTex/SwiftTex.h
@@ -0,0 +1,18 @@
+//
+//  SwiftTex.h
+//  SwiftTex
+//
+//  Created by Adam Wulf on 11/30/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftTex.
+FOUNDATION_EXPORT double SwiftTexVersionNumber;
+
+//! Project version string for SwiftTex.
+FOUNDATION_EXPORT const unsigned char SwiftTexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTex/PublicHeader.h>
+
+
