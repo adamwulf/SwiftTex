3b9034fcd9ccac7b15095ae1091b319d3cfa1fd7
Adam Wulf <adam.wulf@gmail.com>
2020-12-07 04:45:42 -0600

    Can show a math formula in a document window

--- Formulas/ViewController.swift
+++ Formulas/ViewController.swift
@@ -6,13 +6,25 @@
 //
 
 import Cocoa
+import iosMath
 
 class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let label = MTMathUILabel()
+        label.latex = "x = \\frac{-b \\pm \\sqrt{b^2-4ac}}{2a}"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .textBackgroundColor
+        label.textColor = .textColor
+        label.textAlignment = .center
+        self.view.addSubview(label)
+
+        label.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        label.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        label.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        label.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
     }
 
     override var representedObject: Any? {
