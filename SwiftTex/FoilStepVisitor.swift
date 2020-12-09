d8fa062a71753d152c50db2aecc65b7d40b87f1a
Adam Wulf <adam.wulf@gmail.com>
2020-12-09 01:49:56 -0600

    Can expand a formula out, including exponents

--- SwiftTex/FoilStepVisitor.swift
+++ SwiftTex/FoilStepVisitor.swift
@@ -15,7 +15,24 @@ public class FoilVisitor: IdentityVisitor {
         switch item {
         case let item as BinaryOpNode:
             let multOp = Token.Symbol.mult(implicit: true)
-            if
+            if item.op == .exp {
+                if let exponent = item.rhs as? NumberNode {
+                    if exponent.value < 0 {
+                        let updated = BinaryOpNode(op: .exp, lhs: item.lhs, rhs: -exponent, startToken: item.startToken)
+                        let div = BinaryOpNode(op: .div, lhs: NumberNode(string: "1", startToken: exponent.startToken),
+                                               rhs: updated,
+                                               startToken: item.startToken)
+                        return singleStep ? div : div.accept(visitor: self)
+                    } else if exponent.value == 1 {
+                        return singleStep ? item.lhs : item.lhs.accept(visitor: self)
+                    } else if exponent.value > 2 && exponent.fractionalDigits == 0 {
+                        let updated = BinaryOpNode(op: .exp, lhs: item.lhs, rhs: exponent - 1, startToken: item.startToken)
+                        return BinaryOpNode(op: .mult(implicit: true), lhs: item.lhs, rhs: updated, startToken: item.startToken)
+                    } else if exponent.value > 1 && exponent.fractionalDigits == 0 {
+                        return BinaryOpNode(op: .mult(implicit: true), lhs: item.lhs, rhs: item.lhs, startToken: item.startToken)
+                    }
+                }
+            } else if
                 item.op == .mult(),
                 let factor = item.lhs as? BinaryOpNode,
                 factor.op == .plus || factor.op == .minus {
